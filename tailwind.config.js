38954de3689031cd9255a8feebed29468d31312e
Gaurav Jakhar <jakhargaurav001@gmail.com>
2025-01-19 15:46:14 +0530

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+        fontFamily: {
+        mullish: ["Mulish","sans-serif"],
+        },
+    
+      colors: {
+      deepBlue: "#02042a", 
+      LightBlue: "#2b84ea",
+      lightBlue300: "#4b94ed",
+      lightBlue500: "#03a9f4",
+      greenLight: "#61cea6", 
+      grayText: "#818597", 
+      LightGray: "#e2e2e2",
+      grayBlue: "#6699CC",
+      deepBlueHead: "#162f56", 
+      gray2: "#525a76",
+    },
+  },
+},
+  plugins: [],
+}
+
