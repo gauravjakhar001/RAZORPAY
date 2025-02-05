fce40f97931cb2e3eddeabc3ab1b36d369a59ec0
Gaurav Jakhar <jakhargaurav001@gmail.com>
2025-02-05 18:24:56 +0530

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,23 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*"],
+  content: ["./index.html", "./src/**/*.{html,js,jsx,ts,tsx}"], // Adjust based on your project structure
   theme: {
     extend: {
-        fontFamily: {
-        mullish: ["Mulish","sans-serif"],
-        },
-    
+      fontFamily: {
+        mullish: ["Mulish", "sans-serif"],
+      },
       colors: {
-      deepBlue: "#02042a", 
-      LightBlue: "#2b84ea",
-      lightBlue300: "#4b94ed",
-      lightBlue500: "#03a9f4",
-      greenLight: "#61cea6", 
-      grayText: "#818597", 
-      LightGray: "#e2e2e2",
-      grayBlue: "#6699CC",
-      deepBlueHead: "#162f56", 
-      gray2: "#525a76",
+        deepBlue: "#02042a",
+        LightBlue: "#2b84ea",
+        lightBlue300: "#4b94ed",
+        lightBlue500: "#03a9f4",
+        greenLight: "#61cea6",
+        grayText: "#818597",
+        LightGray: "#e2e2e2",
+        grayBlue: "#6699CC",
+        deepBlueHead: "#162f56",
+        gray2: "#525a76",
+      },
     },
   },
-},
   plugins: [],
-}
-
+};
